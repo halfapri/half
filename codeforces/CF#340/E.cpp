803845e1f9999ea47708c182e71eed83bf509a02
halfapri <halfcot@gmail.com>
2016-01-24 20:30:05 +0800

    E is not what I imagine

--- codeforces/CF#340/E.cpp
+++ codeforces/CF#340/E.cpp
@@ -2,54 +2,47 @@
 using namespace std;
 const int M = 1e6+10 ;
 int n , m , k ;
-vector<int>g[M] ;
+int cnt[M] ;
 int pre[M] , ure[M] ;
 int a[M] ;
 
 int main () {
 	cin >> n >> m >> k ;
+	int tmp = 0 ;
+	int ans = 0 ;
 	for (int i = 1 ; i <= n ; i ++) {
-		int x ;
 		scanf ("%d" , &a[i]) ;
-		g[a[i]].push_back (i) ;
 	}
-
-	for (int i = 2 ; i <= n ; i ++) {
-		int tmp = k^a[i] ;
-		int x = lower_bound (g[tmp].begin(),g[tmp].end(),i) - g[tmp].begin() ;
-		pre[i] = pre[i-1] ;
-		printf ("i=%d , x=%d\n" , i , x) ;
-		if (g[tmp].size() == 0) continue ;
-		if (x == 0) continue ;
-		pre[i] += x ;
-		printf ("pre[%d]=%d\n" , i , pre[i]) ;
+	cnt[0] = 1 ;
+	for (int i = 1 ; i <= n ; i ++) {
+		tmp = tmp^a[i] ;
+		ans = tmp^k ;
+		//printf ("tmp = %d\n" , tmp) ;
+		//printf ("cnt[%d] = %d\n" , ans , cnt[ans]) ; 
+		pre[i] = pre[i-1] + cnt[ans] ;
+		cnt[tmp] ++ ;
 	}
-	puts ("Pre:") ;
+	memset (cnt , 0 , sizeof(cnt)) ;
+	tmp = 0 ;
+	cnt[0] = 1 ;
+	for (int i = n ; i >= 1 ; i --) {
+		tmp = tmp^a[i] ;
+		ans = tmp^k ;
+		ure[i] = ure[i+1] + cnt[ans] ;
+		cnt[tmp] ++ ;
+	}
+	puts ("") ;
+	puts ("pre:") ;
 	for (int i = 1 ; i <= n ; i ++) printf ("%d " , pre[i]) ; puts ("") ;
-	for (int i = n-1 ; i >= 1 ; i --) {
-		int tmp = k^a[i] ;
-		int x = lower_bound(g[tmp].begin(),g[tmp].end(),i) - g[tmp].begin() ;
-		ure[i] = ure[i+1] ;
-		if (x == g[tmp].size()) continue ;
-		ure[i] += g[tmp].size()-x ;
-	}
-	puts ("Ure:") ;
+	puts ("ure") ;
 	for (int i = 1 ; i <= n ; i ++) printf ("%d " , ure[i]) ; puts ("") ;
 	while (m --) {
-		int l , r ;
-		scanf ("%d%d" , &l , &r) ;
-		int hou_in = ure[r+1] ;
-		int hou_out = pre[n]-pre[r]-hou_in ; 
-		int qian_in = pre[l-1] ;
-		int qian_out = ure[1]-ure[l]-qian_in ;
-		printf ("hou_out=%d,qian_out=%d\n" , hou_out , qian_out) ;
-		
-		int tmp = abs(hou_out-qian_out) ;
-		int ans = ure[l]-ure[r+1] + pre[r]-pre[l-1];
-		cout << "tmp = " << tmp << " ans = " << ans << endl ;
+		int L , R ;
+		scanf ("%d%d" , &L , &R) ;
+		int tmp1 = pre[n]-pre[R]-ure[R+1] ;
+		int tmp2 = ure[1]-ure[L]-pre[L+1] ;
 
-		int f = (ans-tmp)/2 ;
-		cout << f << endl ;
+		cout << pre[R]-pre[L-1] << endl ;
 	}
 	return 0 ;
 }
