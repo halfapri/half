d2c2ef6714a8eb8ddf0ffe76e8e6eb156747fec4
halfapri <halfcot@gmail.com>
2016-01-26 21:13:37 +0800

    I should careful with the border

--- codeforces/CF#340/E.cpp
+++ codeforces/CF#340/E.cpp
@@ -1,48 +1,68 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int M = 1e6+10 ;
+#include <bits/stdc++.h>
+using namespace std ;
+typedef long long ll ;
+#define F first
+#define S second
+#define lson (o<<1),l,mid
+#define rson (o<<1|1),mid+1,r
+const int M = 1e5+10;
 int n , m , k ;
-int cnt[M] ;
-int pre[M] , ure[M] ;
 int a[M] ;
+ll res[M] ;
+int cnt[1<<20] ;
+
+struct query {
+	int l , r , id ;
+} q[M] ;
+int bk ;
+
+bool cmp (query a , query b) {
+	if (a.l/bk == b.l/bk) return a.r<b.r ;
+	return a.l/bk < b.l/bk ;
+}
+
+void solve () {
+	ll tot = 0 ;
+	int l = 1 , r = 0 ;
+	cnt[0] = 1 ;
+	for (int i = 0 ; i < m ; i ++) {
+		while (r < q[i].r) {
+			r ++ ;
+			tot += cnt[a[r]^k] ;
+			cnt[a[r]] ++ ;
+		}
+		while (r > q[i].r) {
+			cnt[a[r]] -- ;
+			tot -= cnt[a[r]^k] ;
+			r -- ;
+		}
+		while (l < q[i].l) {
+			cnt[a[l-1]] -- ;
+			tot -= cnt[a[l-1]^k] ;
+			l ++ ;
+		}
+		while (l > q[i].l) {
+			l -- ;
+			tot += cnt[a[l-1]^k] ;
+			cnt[a[l-1]] ++ ;
+		}
+		res[q[i].id] = tot ;
+	}
+	for (int i = 0 ; i < m ; i ++) printf ("%I64d\n" , res[i]) ;
+}
 
 int main () {
-	cin >> n >> m >> k ;
-	int tmp = 0 ;
-	int ans = 0 ;
+	cin>>n>>m>>k ;
+	bk = sqrt(1.0*n) ;
 	for (int i = 1 ; i <= n ; i ++) {
 		scanf ("%d" , &a[i]) ;
 	}
-	cnt[0] = 1 ;
-	for (int i = 1 ; i <= n ; i ++) {
-		tmp = tmp^a[i] ;
-		ans = tmp^k ;
-		//printf ("tmp = %d\n" , tmp) ;
-		//printf ("cnt[%d] = %d\n" , ans , cnt[ans]) ; 
-		pre[i] = pre[i-1] + cnt[ans] ;
-		cnt[tmp] ++ ;
+	for (int i = 0 ; i < m ; i ++) {
+		scanf ("%d%d" , &q[i].l,&q[i].r) ;
+		q[i].id = i ;
 	}
-	memset (cnt , 0 , sizeof(cnt)) ;
-	tmp = 0 ;
-	cnt[0] = 1 ;
-	for (int i = n ; i >= 1 ; i --) {
-		tmp = tmp^a[i] ;
-		ans = tmp^k ;
-		ure[i] = ure[i+1] + cnt[ans] ;
-		cnt[tmp] ++ ;
-	}
-	puts ("") ;
-	puts ("pre:") ;
-	for (int i = 1 ; i <= n ; i ++) printf ("%d " , pre[i]) ; puts ("") ;
-	puts ("ure") ;
-	for (int i = 1 ; i <= n ; i ++) printf ("%d " , ure[i]) ; puts ("") ;
-	while (m --) {
-		int L , R ;
-		scanf ("%d%d" , &L , &R) ;
-		int tmp1 = pre[n]-pre[R]-ure[R+1] ;
-		int tmp2 = ure[1]-ure[L]-pre[L+1] ;
-
-		cout << pre[R]-pre[L-1] << endl ;
-	}
+	sort (q,q+m,cmp) ;
+	for (int i = 1 ; i <= n ; i ++) a[i] ^= a[i-1] ;
+	solve () ;
 	return 0 ;
 }
