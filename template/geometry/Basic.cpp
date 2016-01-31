c02b93358b6b1928875a9f6569fce0da5eb75b2a
halfapri <halfcot@gmail.com>
2016-01-31 17:34:04 +0800

    Basic

--- template/geometry/Basic.cpp
+++ template/geometry/Basic.cpp
@@ -1,9 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define pi (acos(-1.0)) 
+#include <bits/stdc++.h>
+using namespace std ;
 typedef long long ll ;
-
+#ifdef _WIN32
+#define LLD "%I64d"
+#else
+#define LLD "%lld"
+#endif
+#define pi (acos(-1.0))
+#define F first
+#define S second
+#define lson (o<<1),l,mid
+#define rson (o<<1|1),mid+1,r
+#define MP make_pair
 const double eps = 1e-9 ;
+const int inf = 0x3f3f3f3f ;
+const ll INF = (ll)1e18 ;
 
 int sign(double x) {return x < -eps ? -1 : x > eps ;}
 struct Point {
