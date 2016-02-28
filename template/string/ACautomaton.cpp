9acc3ed579c08e8e543b02132ef927ad4689b87c
halfapri <halfcot@gmail.com>
2016-02-28 16:48:38 +0800

    Aho-Corasick automation

--- template/string/ACautomaton.cpp
+++ template/string/ACautomaton.cpp
@@ -19,54 +19,86 @@ const int inf = 0x3f3f3f3f ;
 const ll INF = (ll)4e18 ;
 
 struct Trie {
-	const int M = 100000+10 ;
-	int go[M][10] , fail[M] ;
-	bool end[M] ;
+#define M (500000+10)
+	int go[M][26] , fail[M] , end[M] ;
 	int o , root ;
 
+	void init () {
+		o = 0 ;
+		root = newNode () ;
+	}
+
 	int newNode () {
-		for (int i=0 ; i<10 ; i++)
+		for (int i=0 ; i<26 ; i++)
 			go[o][i] = -1 ;
 		end[o] = false ;
 		return o ++ ;
 	}
 
-	void Init () {
-		o = 0 ;
-		root = newNode() ;
-	}
-
 	void Insert (char *s) {
 		int now = root ;
 		for (; *s ; s ++) {
-			if (go[now][*s-'0']==-1) 
-				go[now][*s-'0'] = newNode () ;
-			now = go[now][*s-'0'] ;
+			if (go[now][*s-'a']==-1)  
+				go[now][*s-'a'] = newNode () ;
+			now = go[now][*s-'a'] ;
 		}
-		end[now] = true ;
+		end[now] ++ ;
 	}
 
-	void Build () {
+	void get_fail () {
 		queue<int> q ;
-		for (int i=0 ; i<10 ; i++) {
-			if (go[root][i]==-1) 
+		for (int i=0 ; i<26 ; i++) {
+			if (go[root][i]==-1)
 				go[root][i] = root ;
 			else {
 				fail[go[root][i]] = root ;
-				q.push(go[root][i]) ;
+				q.push (go[root][i]) ;
 			}
 		}
-		while (!q.empty()) {
-			int now = q.front() ; q.pop () ;
-			end[now] |= end[fail[now]] ;
-			for (int i=0 ; i<10 ; i++) {
-				if (go[now][i]==-1) 
+		while (!q.empty ()) {
+			int now=q.front() ; q.pop() ;
+			for (int i=0 ; i<26 ; i++) {
+				if (go[now][i] == -1) 
 					go[now][i] = go[fail[now]][i] ;
-				else {
+				else { 
 					fail[go[now][i]] = go[fail[now]][i] ;
 					q.push (go[now][i]) ;
 				}
 			}
 		}
 	}
-} ;
+
+	int search (char *s) {
+		int cnt = 0 ;
+		int now = root ;
+		for (; *s ; s ++) {
+			now = go[now][*s-'a'] ;
+			int tmp = now ;
+			while (tmp != root) {
+				cnt += end[tmp] ;
+				end[tmp] = 0 ;
+				tmp = fail[tmp] ;
+			}
+		}
+		return cnt ;
+	}
+} ac ;
+char s[1000000+10] ;
+int n ;
+
+int main () {
+	int T ;
+	scanf ("%d" , &T) ;
+	while (T --) {
+		ac.init () ;
+		scanf ("%d" , &n) ;
+		for (int i = 0 ; i < n ; i ++) {
+			scanf ("%s" , s) ;
+			ac.Insert (s) ;
+		}
+		ac.get_fail () ;
+		scanf ("%s" , s) ;
+		printf ("%d\n" , ac.search (s)) ;
+	}
+	return 0 ;
+}
