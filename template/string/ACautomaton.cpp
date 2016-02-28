df1d0a158af9dd9c284904d3251915f3aa719c15
halfapri <halfcot@gmail.com>
2016-02-28 13:40:16 +0800

    Aho-Corasick automation

--- template/string/ACautomaton.cpp
+++ template/string/ACautomaton.cpp
@@ -20,13 +20,13 @@ const ll INF = (ll)4e18 ;
 
 struct Trie {
 	const int M = 100000+10 ;
-	int nxt[M][10] , fail[M] ;
+	int go[M][10] , fail[M] ;
 	bool end[M] ;
 	int o , root ;
 
 	int newNode () {
 		for (int i=0 ; i<10 ; i++)
-			nxt[o][i] = -1 ;
+			go[o][i] = -1 ;
 		end[o] = false ;
 		return o ++ ;
 	}
@@ -39,9 +39,9 @@ struct Trie {
 	void Insert (char *s) {
 		int now = root ;
 		for (; *s ; s ++) {
-			if (nxt[now][*s-'0']==-1) 
-				nxt[now][*s-'0'] = newNode () ;
-			now = nxt[now][*s-'0'] ;
+			if (go[now][*s-'0']==-1) 
+				go[now][*s-'0'] = newNode () ;
+			now = go[now][*s-'0'] ;
 		}
 		end[now] = true ;
 	}
@@ -49,22 +49,22 @@ struct Trie {
 	void Build () {
 		queue<int> q ;
 		for (int i=0 ; i<10 ; i++) {
-			if (nxt[root][i]==-1) 
-				nxt[root][i] = root ;
+			if (go[root][i]==-1) 
+				go[root][i] = root ;
 			else {
-				fail[nxt[root][i]] = root ;
-				q.push(nxt[root][i]) ;
+				fail[go[root][i]] = root ;
+				q.push(go[root][i]) ;
 			}
 		}
 		while (!q.empty()) {
 			int now = q.front() ; q.pop () ;
 			end[now] |= end[fail[now]] ;
 			for (int i=0 ; i<10 ; i++) {
-				if (nxt[now][i]==-1) 
-					nxt[now][i] = nxt[fail[now]][i] ;
+				if (go[now][i]==-1) 
+					go[now][i] = go[fail[now]][i] ;
 				else {
-					fail[nxt[now][i]] = nxt[fail[now]][i] ;
-					q.push (nxt[now][i]) ;
+					fail[go[now][i]] = go[fail[now]][i] ;
+					q.push (go[now][i]) ;
 				}
 			}
 		}
