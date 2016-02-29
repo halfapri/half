b2c628d9ca2466a71983d4b80dba3fa681561692
halfapri <halfcot@gmail.com>
2016-02-29 21:20:16 +0800

    二分区间+线段树

--- borc/segtree/zstu4186/A.cpp
+++ borc/segtree/zstu4186/A.cpp
@@ -1,9 +1,23 @@
-#include<bits/stdc++.h>
-#define lson o<<1,l,mid
-#define rson o<<1|1,mid+1,r
+#include <bits/stdc++.h>
+using namespace std ;
+typedef long long ll ;
+typedef long double ld ;
+typedef unsigned long long ull ;
+#ifdef _WIN32
+#define LLD "%I64d"
+#else
+#define LLD "%lld"
+#endif
+#define pi (acos(-1.0))
 #define F first
 #define S second
-using namespace std;
+#define lson (o<<1),l,mid
+#define rson (o<<1|1),mid+1,r
+#define MP make_pair
+const double eps = 1e-9 ;
+const int inf = 0x3f3f3f3f ;
+const ll INF = (ll)4e18 ;
+
 const int M = 8e4 + 10 ;
 typedef pair<int,int> pii ;
 vector<pii> sis[M] , node[M<<2] ;
