ef533443d23c829f8656b37c81db7e0758a1f0fb
halfapri <halfcot@gmail.com>
2016-05-14 14:30:28 +0800

    字典树

--- template/string/trie.cpp
+++ template/string/trie.cpp
@@ -35,3 +35,37 @@ int main () {
 	}
 	return 0 ;
 }
+/////////////////////////////////////////////////////////////////////////////////////
+struct TRIE {
+	int go[M][26] ;
+   	bool ed[M] ;
+	int root , o ;
+
+	void init () {
+		o = 0 ;
+		root = newnode () ;
+	}
+	int newnode () {
+		for (int i=0 ; i<26 ; i++) go[o][i] = -1 ;
+		ed[o] = 0 ;
+		return o ++ ;
+	}
+	void Insert (char *s,int rt=0) {
+		for (; *s ; s++) {
+			int ch = *s-'a' ;
+			if (go[rt][ch]==-1) {
+				go[rt][ch] = newnode () ;
+			}
+			rt = go[rt][ch] ;
+		}
+		ed[rt] = 1 ;
+	}
+	bool search (char *s,int rt=0) {
+		for (; *s ; s++) {
+			int ch = *s-'a' ;
+			if (go[rt][ch]==-1) return false ;
+			rt = go[rt][ch] ;
+		}
+		return ed[rt] ;
+	}
+} trie ;
