d767ac0325316e3d20b5a2f0cadbb402b19e73f2
halfapri <halfcot@gmail.com>
2016-05-14 16:43:21 +0800

    单调栈

--- template/data_structure/Monotone_Priority_Stack.cpp
+++ template/data_structure/Monotone_Priority_Stack.cpp
@@ -1,47 +1,21 @@
-/*
-   这里求 ai 的区间[Li , Ri] ,使得在这个区间里 ai 是最小的.
-   当出现 2 1 2 1 1 2 2这种情况时，我们令靠前的 2 统治的区间尽可能大.
-*/
 #include<bits/stdc++.h>
 using namespace std;
 const int M = 1e5 + 7 ;
 int L[M] , R[M] ;
 int n ;
-int a[M] ;
+int h[M] ;
 
-void L_wall () {
-	stack<int> wall ;
-	L[1] = 1 ;
-	wall.push (1) ;
-	for (int i = 2 ; i <= n ; i ++) {
-		if (a[wall.top()] < a[i]) L[i] = wall.top()+1 ; 
-		else {
-			while (!wall.empty() && a[wall.top()] >= a[i]) wall.pop () ; 
-			if (!wall.empty ()) L[i] = 1 ;
-			else L[i] = wall.top () + 1 ;
-		}
-		wall.push (i) ;
+void run () {
+	stack<int> q ;
+	for (int i=0 ; i<n ; i++) {
+		while (!q.empty() && h[q.top()]>=h[i]) q.pop() ;
+		L[i] = q.empty()?0:q.top()+1 ;
+		q.push(i) ;
 	}
-}
-
-void R_wall () {
-	stack<int> wall ;
-	R[n] = n ; 
-	wall.push (n) ;
-	for (int i = n-1 ; i >= 1 ; i --) {
-		if (a[wall.top()] <= a[i]) R[i] = wall.top()-1 ; 
-		else {
-			while (!wall.empty() && a[wall.top()] > a[i]) wall.pop () ;
-			if (!wall.empty ()) R[i] = n ;
-			else R[i] = wall.top()-1 ;
-		}
-		wall.push (i) ;
+	while (!q.empty()) q.pop() ;
+	for (int i=n-1 ; i>=0 ; i--) {
+		while(!q.empty() && h[q.top()]>=h[i]) q.pop() ;
+		R[i] = q.empty()?n-1:q.top()-1 ;
+		q.push(i) ;
 	}
-}
-
-int main () {
-	scanf ("%d" , &n) ;
-	for (int i = 1 ; i <= n ; i ++) 
-		scanf ("%d" , &a[i]) ;
-	return 0 ;
 }
