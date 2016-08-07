bb44e686b1629bfa68b9690908064acf8a5a4317
halfapri <halfcot@gmail.com>
2016-08-07 11:59:59 +0800

    manacher

--- template/string/manacher.cpp
+++ template/string/manacher.cpp
@@ -1,34 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std ;
-typedef long long ll ;
-typedef long double ld ;
-typedef unsigned long long ull ;
-#ifdef _WIN32
-#define LLD "%I64d"
-#else
-#define LLD "%lld"
-#endif
-#define pi (acos(-1.0))
-#define F first
-#define S second
-#define lson (o<<1),l,mid
-#define rson (o<<1|1),mid+1,r
-#define MP make_pair
-const double eps = 1e-9 ;
-const int inf = 0x3f3f3f3f ;
-const ll INF = (ll)1e18 ;
-
-const int M = 100000+10 ;
-char s[M] ;
-int pal[M<<1] ;
-
+char s[100000] ;
 void manacher () {
-	int lens = strlen (s) ;
-	int mx=0 , id=0 ;
-	for (int i=0 , j; i<lens*2 ; i ++) {
-		if (i>mx) j=1 ; else j=min(pal[2*id-i],mx-i) ;
-		while (i-j>=0&&i+j<lens*2&&((i+j)&1 || s[i+j>>1]==s[i-j>>1])) j ++ ;
-		pal[i] = j ;
-		if (i+pal[i]>mx) mx=i+pal[i],id=i ;
+	lens = strlen(s) ;
+	lent = 0 ;
+	t[lent++] = '$' , t[lent++]='#' ;
+	for (int i=0 ; i<lens ; i++) {
+		t[lent++] = s[i] ;
+		t[lent++] = '#' ;
+	}
+
+	int mx=0 ,id=0 ;
+	for (int i=1 ; i<lent ; i++) {
+		if (mx>i) pal[i] = pal[2*id-i]<mx-i?pal[2*id-i]:mx-i ;
+		else pal[i] = 1 ;
+		while (t[i+pal[i]]==t[i-pal[i]]) pal[i]++ ;
+		if (i+pal[i]>mx) {
+			mx = i+pal[i] ;
+			id = i ;
+		}
 	}
 }
+
