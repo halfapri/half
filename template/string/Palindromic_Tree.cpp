fdd0d4774a6ab99c7d1e3811c51b1e52fb7bce20
halfapri <halfcot@gmail.com>
2016-08-07 12:02:56 +0800

    回文自动机

--- template/string/Palindromic_Tree.cpp
+++ template/string/Palindromic_Tree.cpp
@@ -1,71 +1,72 @@
-#include <bits/stdc++.h>
-using namespace std ;
-typedef long long ll ;
-typedef long double ld ;
-typedef unsigned long long ull ;
-#ifdef _WIN32
-#define LLD "%I64d"
-#else
-#define LLD "%lld"
-#endif
-#define pi (acos(-1.0))
-#define F first
-#define S second
-#define lson (o<<1),l,mid
-#define rson (o<<1|1),mid+1,r
-#define MP make_pair
-const double eps = 1e-9 ;
-const int inf = 0x3f3f3f3f ;
-const ll INF = (ll)4e18 ;
-
-const int M = 1e5 ;
-struct Palindromic {
-	int S[M] ;//1based
-	int sL , oL , last ;
-	int go[M][26] ;
-	int fail[M] ;
-	int len[M] ; //每种回文串的长度
-	int pal[M] ; //不同类型的回文串的数量
-	int num[M] ; //以i下标作为结尾的回文串的数量
-
-	int newnode (int l) {
-		for (int i=0 ; i<26 ; i++) go[pL][i]=0 ;
-		pal[pL]=0 ;
-		len[pL]=l ;
-		num[pL]=0 ;
-		return pL ++ ;
-	}
-
-	void init () {
-		sL=pL=0 ;
-		newnode (0) ;//偶数回文串的根
-		newnode (-1) ;//奇数回文串的根
-		s[sL++] = '$';
-		last = 1 ;
-		fail[0] = 1 ;
-	}
-
-	int get_fail (int x) {
-		while (S[sL-len[x]-1] != S[sL]) x = fail[x] ;
-		return x;
-	}
-		
-	void Insert (char v) {
-		v -= 'a' ;
-		S[++sL] = v ;
-		int cur = get_fail (last) ;
-		if (!go[cur][v]) {
-			int now = newnode(len[cur]+2) ;
-			fail[now] = go[get_fail(fail[cur])][v] ;
-			go[cur][v] = now ;
-			num[sL] = num[fail[now]] + 1 ;
-		}
-		last = go[cur][v] ;
-		pal[last] ++ ;
-	}
-
-	void count () {
-		for (int i=oL-1 ; i>=0 ; i--)
-			cnt[fail[i]] += cnt[i] ;
-	}
-} pt;
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN = 100005 ;
+const int N = 26 ;
+char s[MAXN];
+struct Palindromic_Tree
+{
+    int next[MAXN][N] ;//next指针，next指针和字典树类似，指向的串为当前串两端加上同一个字符构成
+    int fail[MAXN] ;//fail指针，失配后跳转到fail指针指向的节点
+    int cnt[MAXN] ;
+    int num[MAXN] ; // 当前节点通过fail指针到达0节点或1节点的步数(fail指针的深度)
+    int len[MAXN] ;//len[i]表示节点i表示的回文串的长度
+    int S[MAXN] ;//存放添加的字符
+    int last ;//指向上一个字符所在的节点，方便下一次add
+    int n ;//字符数组指针
+    int p ;//节点指针
+    int newnode(int l)     //新建节点
+    {
+          for(int i = 0 ; i < N ; ++ i) next[p][i] = 0 ;
+          cnt[p] = 0 ;
+          num[p] = 0 ;
+          len[p] = l ;
+          return p ++ ;
+    }
+    void init()   //初始化
+    {
+          p = 0 ;
+          newnode(0) ;
+          newnode(-1) ;
+          last = 0 ;
+          n = 0 ;
+          S[n] = -1 ;//开头放一个字符集中没有的字符，减少特判
+          fail[0] = 1 ;
+    }
+    int get_fail(int x)     //和KMP一样，失配后找一个尽量最长的
+    {
+          while(S[n - len[x] - 1] != S[n]) x = fail[x] ;
+          return x ;
+    }
+    void add(int c,int pos)
+    {
+          printf("%d:",p);
+          c -= 'a';
+          S[++ n] = c ;
+          int cur = get_fail(last) ;   //通过上一个回文串找这个回文串的匹配位置
+          printf("%d ",cur);
+          if(!next[cur][c])     //如果这个回文串没有出现过，说明出现了一个新的本质不同的回文串
+          {
+                int now = newnode(len[cur] + 2) ;   //新建节点
+                fail[now] = next[get_fail(fail[cur])][c] ;   //和AC自动机一样建立fail指针，以便失配后跳转
+                next[cur][c] = now ;
+                num[now] = num[fail[now]] + 1 ;
+                for(int i=pos-len[now]+1; i<=pos; ++i) printf("%c",s[i]);
+          } last = next[cur][c] ;
+          cnt[last] ++ ;
+          putchar(10);
+    }
+    void count()
+    {
+          for(int i = p - 1 ; i >= 0 ; -- i) cnt[fail[i]] += cnt[i] ;
+          //父亲累加儿子的cnt，因为如果fail[v]=u，则u一定是v的子回文串！
+    }
+} run;
+int main()
+{
+    scanf("%s",&s);
+    int n=strlen(s);
+    run.init();
+    for(int i=0; i<n; i++) run.add(s[i],i);
+    run.count();
+    return 0;
+}
