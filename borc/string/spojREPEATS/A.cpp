406adaf3d6c8065c39965a3fd29b3d7150591185
halfapri <halfcot@gmail.com>
2016-08-18 09:08:14 +0800

    后缀数组，最大局部循环节个数

---
+++ borc/string/spojREPEATS/A.cpp
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <bitset>
+#include <string.h>
+#include <stack>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <complex>
+#include <functional>
+using namespace std ;
+typedef long long ll ;
+typedef long double ld ;
+typedef unsigned long long ull ;
+#ifdef _WIN32
+#define LLD "%I64d"
+#else
+#define LLD "%lld"
+#endif
+#define pi (acos(-1.0))
+#define fi first
+#define se second
+#define lson (o<<1),l,mid
+#define rson (o<<1|1),mid+1,r
+#define MP make_pair
+#define sqr(x) ((x)*(x))
+#define ALL(v) (v).begin(),(v).end()
+#define showtime fprintf(stderr,"time = %.15f\n",clock() / (double)CLOCKS_PER_SEC)
+const double eps = 1e-9 ;
+const int inf = 0x3f3f3f3f ;
+const ll INF = (ll)4e18 ;
+const int MOD=(int)1e9+7,BAS=257,invBAS=70038911;
+inline int sign(double x){return x<-eps?-1:x>eps;}
+inline int clz(int x){return __builtin_clz(x);}
+inline int clz(ll x){return __builtin_clzll(x);}
+inline int lg2(int x){return !x ? -1 : 31-clz(x);}
+inline int lg2(ll x){return !x ? -1 : 63-clz(x);}
+
+const int M = 50000+10;
+char s[M];
+int sa[M], height[M], rk[M], st[18][M], n; 
+
+void da (const char *s, int n, int m) {
+	static int t1[M], t2[M], c[M];
+	int *x=t1, *y=t2, i,j,k,p=1;
+	memset (c, 0, sizeof(c[0])*m);
+	for (i=0; i<n; i++) c[x[i]=s[i]] ++; 
+	for (i=1; i<m; i++) c[i] += c[i-1];
+	for (i=n-1; i>=0; i--) sa[--c[x[i]]] = i;
+	for (k=1; p<n; k<<=1, m=p) {
+		for (p=0, i=n-k; i<n; i++) y[p++] = i;
+		for (i=0; i<n; i++) if(sa[i]>=k) y[p++] = sa[i]-k;
+		memset (c, 0, sizeof(c[0])*m);
+		for (i=0; i<n; i++) c[x[i]] ++;
+		for (i=1; i<m; i++) c[i] += c[i-1];
+		for (i=n-1; i>=0; i--) sa[--c[x[y[i]]]] = y[i];  
+		for (swap(x,y), x[sa[0]]=0, p=i=1; i<n; i++)
+			x[sa[i]] = y[sa[i]] == y[sa[i-1]] &&
+				y[sa[i]+k] == y[sa[i-1]+k] ? p-1: p++;
+	}
+	memcpy (rk, x, sizeof(x[0])*n);
+	for (i=0, k=0; i<n-1; height[rk[i++]] = k?k--:k) 
+		for (j=sa[rk[i]-1]; s[i+k]==s[j+k]; k++);
+	//memset (st, 0, sizeof(st));
+	memcpy (st[0], height, sizeof(height[0])*n);
+	for (k=1; (1<<k)<=n-1; k++) 
+		for (i=0; i+(1<<k)-1<n-1; i++) 
+			st[k][i] = min (st[k-1][i], st[k-1][i+(1<<k-1)]);
+}
+
+int lcp (int i,int j) {
+	i=rk[i], j=rk[j];
+	if (i>j) swap (i,j);
+	i ++;
+	int f = lg2(j-i+1);
+	return min (st[f][i], st[f][j-(1<<f)+1]);
+}
+
+int main () {
+	//freopen ("read.txt", "r", stdin);
+	int CASE;
+	scanf ("%d", &CASE);
+	while (CASE --) {
+		scanf ("%d", &n);
+		for (int i=0; i<n; i++) scanf ("%s", s+i); 
+		s[n] = '\0';
+		da (s, n+1, 256);
+		int maxn = 1;
+	//	for (int i=0; i<n; i++) printf ("%-3d", a[i]); puts ("");
+	//	for (int i=0; i<n; i++) printf ("%-3d", rk[i]); puts("");
+	//	for (int i=0; i<n; i++) printf ("%-3d", height[i]);puts("");
+		for (int i=1; i<=n; i++) {
+			for (int j=0; j+i<n; j+=i) {
+				int dis = lcp(j, j+i);
+				int k = (i+dis)/i;
+				int xk = i-dis%i;
+				if (dis%i!=0 && j-xk>=0) {
+					dis = lcp(j-xk, j-xk+i);
+					k = max (k , (i+dis)/i);
+				}
+				maxn = max(maxn, k);
+			}
+		}
+		printf ("%d\n", maxn);
+	}
+	return 0;
+}
---
+++ borc/string/spojREPEATS/test.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <bitset>
+#include <string.h>
+#include <stack>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <complex>
+#include <functional>
+using namespace std ;
+typedef long long ll ;
+typedef long double ld ;
+typedef unsigned long long ull ;
+#ifdef _WIN32
+#define LLD "%I64d"
+#else
+#define LLD "%lld"
+#endif
+#define pi (acos(-1.0))
+#define fi first
+#define se second
+#define lson (o<<1),l,mid
+#define rson (o<<1|1),mid+1,r
+#define MP make_pair
+#define sqr(x) ((x)*(x))
+#define ALL(v) (v).begin(),(v).end()
+#define showtime fprintf(stderr,"time = %.15f\n",clock() / (double)CLOCKS_PER_SEC)
+const double eps = 1e-9 ;
+const int inf = 0x3f3f3f3f ;
+const ll INF = (ll)4e18 ;
+const int MOD=(int)1e9+7,BAS=257,invBAS=70038911;
+inline int sign(double x){return x<-eps?-1:x>eps;}
+inline int clz(int x){return __builtin_clz(x);}
+inline int clz(ll x){return __builtin_clzll(x);}
+inline int lg2(int x){return !x ? -1 : 31-clz(x);}
+inline int lg2(ll x){return !x ? -1 : 63-clz(x);}
+
+int main () {
+	printf ("%d %d\n", sizeof(int), sizeof(int));
+	int n;
+	while (~ scanf ("%d", &n)) {
+		printf ("%d %d\n", lg2(n), 1<<lg2(n));
+	}
+	return 0;
+}
