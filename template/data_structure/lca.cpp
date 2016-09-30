664b02bd118bb51880afd34c53974027599a01fd
halfapri <halfcot@gmail.com>
2016-09-30 15:18:10 +0800

    LCA

--- template/data_structure/lca.cpp
+++ template/data_structure/lca.cpp
@@ -1,31 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int M = 1e5 + 10 ;
-int tin[M] , tout[M] , tim ;
-int anc[M][20] ;
+const int M = 100000+10;
+int tin[M],tout[M],tim,deep[M],anc[18][M];
+vector<int> g[M];
 
-vector<int> g[M] ;
-
-void lca (int o , int u) {
-	tin[u] = ++ tim ;
-	for (int v : g[u]) {
-		if (v == o) continue ;
-		anc[v][0] = u ;
-		for (int i = 1 ; i < 20 ; i ++)  {
-			anc[v][i] = anc[ anc[v][i-1] ][ i-1 ] ;
-		}
-		lca (u , v ) ;
+void dfs (int o,int u) {
+	tin[u] = ++tim;
+	for (int v:g[o]) if (v!=o) {
+		deep[v] = deep[u]+1;
+		anc[0][v] = u;
+		for (int k=1; k<18; k++) anc[k][v] = anc[k-1][anc[k-1][v]];
+		dfs (u, v);
 	}
-	tout[u] = ++ tim ;
+	tout[u] = tim;
 }
 
-bool isanc (int u , int v) { return tin[u] <= tin[v] && tout[u] >= tout[v] ; }
-
-int get_lca (int u , int v) {
-	int i = 19 ;
-	while (!isanc(u , v)) {
-		if (i == 0 || !isanc(anc[u][i] , v)) u = anc[u][i] ;
-		else i -- ;
-	}
-	return u ;
+int getlca (int u,int v) {
+	if (deep[u]<deep[v]) swap(u,v);
+	for (int i=17; i>=0; i--)
+		if (deep[anc[i][u]]>=deep[v]) u=anc[i][u];
+	if (u==v) return u;
+	for (int i=17; i>=0; i--)
+		if (anc[i][u]!=anc[i][v]) u=anc[i][u], v=anc[i][v];
+	return anc[0][u];
 }
